4ceaaaea398cb519dc873a07ebc0113915f002fe
wangkun <wangkun@woodemi.com>
2020-11-24 16:34:01 +0800

    Fetch info, show detail and write content
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- lib/gen_license.dart
+++ lib/gen_license.dart
@@ -1,49 +1,27 @@
-import 'package:prompts/prompts.dart' as prompts;
+import 'dart:io';
 
-void choose() {
+import 'package:prompts/prompts.dart' as prompts;
+import 'package:http/http.dart' as http;
+
+part 'licenses.dart';
+
+void choose() async {
   var selection =
       prompts.choose('Which license would you like to generate', licenses);
-  print(selection);
+  var licenseInfo = await http.get(
+      'https://raw.githubusercontent.com/github/choosealicense.com/gh-pages/_licenses/$selection');
+  var split = licenseInfo.body.split('---');
+  _showDetail(selection, split[1]);
+  await _writeContent(split[2]);
 }
 
-const licenses = [
-  '0bsd.txt',
-  'afl-3.0.txt',
-  'agpl-3.0.txt',
-  'apache-2.0.txt',
-  'artistic-2.0.txt',
-  'bsd-2-clause.txt',
-  'bsd-3-clause-clear.txt',
-  'bsd-3-clause.txt',
-  'bsd-4-clause.txt',
-  'bsl-1.0.txt',
-  'cc-by-4.0.txt',
-  'cc-by-sa-4.0.txt',
-  'cc0-1.0.txt',
-  'cecill-2.1.txt',
-  'ecl-2.0.txt',
-  'epl-1.0.txt',
-  'epl-2.0.txt',
-  'eupl-1.1.txt',
-  'eupl-1.2.txt',
-  'gpl-2.0.txt',
-  'gpl-3.0.txt',
-  'isc.txt',
-  'lgpl-2.1.txt',
-  'lgpl-3.0.txt',
-  'lppl-1.3c.txt',
-  'mit.txt',
-  'mpl-2.0.txt',
-  'ms-pl.txt',
-  'ms-rl.txt',
-  'ncsa.txt',
-  'odbl-1.0.txt',
-  'ofl-1.1.txt',
-  'osl-3.0.txt',
-  'postgresql.txt',
-  'unlicense.txt',
-  'upl-1.0.txt',
-  'vim.txt',
-  'wtfpl.txt',
-  'zlib.txt',
-];
+void _showDetail(String selection, String detail) {
+  print('--- $selection detail ---');
+  print(detail);
+}
+
+Future _writeContent(String content) async {
+  print('--- Generation start ---');
+  await File('LICENSE').writeAsString(content);
+  print('--- Generation end ---');
+}
