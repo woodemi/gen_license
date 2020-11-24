b249333ee6283ec7447e754ecd92d9a25acb0bc5
wangkun <wangkun@woodemi.com>
2020-11-24 15:00:29 +0800

    Initial commit
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ bin/gen_license.dart
@@ -0,0 +1,5 @@
+import 'package:gen_license/gen_license.dart' as gen_license;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gen_license.calculate()}!');
+}
---
+++ test/gen_license_test.dart
@@ -0,0 +1,8 @@
+import 'package:gen_license/gen_license.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
